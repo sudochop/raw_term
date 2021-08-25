bc64c2d7f0d36e8d1a1e61131bd9549e4d6b88e9
Michael Miller <mmiller.6@go.ccad.edu>
2021-08-25 15:23:43 -0400

    docs and typespecs

--- lib/raw_term.ex
+++ lib/raw_term.ex
@@ -8,6 +8,7 @@ defmodule RawTerm do
   # escript support
   @priv_dir Application.get_env(:raw_term, :priv_dir, :code.priv_dir(:raw_term))
 
+  @doc false
   def load_nif do
     @priv_dir
     |> Path.join("raw_term")
@@ -15,10 +16,18 @@ defmodule RawTerm do
     |> :erlang.load_nif(0)
   end
 
+  @doc """
+  Enable raw mode
+  """
+  @spec enable() :: :ok | {:error, :tcgetattr | :tcsetattr}
   def enable do
     raise "NIF enable/0 not implemented"
   end
 
+  @doc """
+  Disable raw mode
+  """
+  @spec enable() :: :ok | {:error, :tcsetattr}
   def disable do
     raise "NIF disable/0 not implemented"
   end
